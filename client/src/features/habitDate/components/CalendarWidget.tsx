3d5933272fa19dc719087a43fe1e1eec9236e56a
Nick Stebb <n.stebbings@gmail.com>
2021-11-20 11:30:58 +1300

    Fix ipad alignment issues with cal widge

--- client/src/features/habitDate/components/CalendarWidget.tsx
+++ client/src/features/habitDate/components/CalendarWidget.tsx
@@ -25,17 +25,19 @@ const isMobile = window.matchMedia("only screen and (max-width: 1024px)").matche
     document.querySelector(".mask-wrapper").style.height = "21rem"
     document.getElementById("hamburger").checked = false;
 
-    document.querySelector(".mask-wrapper .wide-nav").style.borderTopRightRadius = '3rem' 
+    document.querySelector(".mask-wrapper .wide-nav").style.borderTopRightRadius = '1.5rem' 
+    document.querySelector(".habit-description-label").style.left = '-3rem'
 
     document.getElementById("current-habit-label-sm").style.borderBottomWidth = '0px'    
   }
 
   const slideOut = (e) => {
     if(!isMobile || e.target.classList.contains("cal-date-nav")) return;
-    e.currentTarget.style.right = "66%";
+    e.currentTarget.style.right = "calc(100% - 4rem)";
     document.querySelector(".mask-wrapper").style.height = "initial"
 
     document.querySelector(".mask-wrapper .wide-nav").style.borderTopRightRadius = '0rem'
+    document.querySelector(".habit-description-label").style.left = '0rem'
 
     document.getElementById("current-habit-label-sm").style.borderBottomWidth = '3px'
   }
@@ -50,13 +52,12 @@ const isMobile = window.matchMedia("only screen and (max-width: 1024px)").matche
   const currentSpace = useAppSelector(selectCurrentSpace);
 
   return (
-    <div className="calendar-widget lg:top-20 top-20 lg:flex lg:right-6 flex-nowrap absolute justify-end w-full h-full pt-8 right-2/3" onClick={toggleSlide} onMouseOver={slideIn} onMouseLeave ={slideOut} >
-      <div className="-left-12  habit-description-label gap-y-2 rounded-3xl text-balance-basic-black xl:flex relative top-0 z-0 flex flex-col items-center w-full overflow-none bg-gray-100 border-4">
+    <div className="calendar-widget lg:top-20 top-20 lg:flex lg:right-6 flex-nowrap absolute justify-end w-full h-full pt-8" onClick={toggleSlide} >
+      <div className="habit-description-label gap-y-2 rounded-3xl text-balance-basic-black xl:flex relative top-0 z-0 flex flex-col items-center w-full overflow-none bg-gray-100 border-4">
         <h2 className="flex mt-4 underline">Description</h2>
         <span className="flex">{currentHabit.meta.description}</span>
         <h2 className="flex mt-1 underline">Initiated On</h2>
         <span className="flex">{stringifyDate(currentHabit.timeframe.fromDate)}</span>
-
           <i                             className="cal-date-nav fa fa-chevron-circle-left text-3xl ml-2 relative -bottom-16 -left-1/4 lg:hidden" onClick={handlePrev} />
         <i                          className="cal-date-nav fa fa-chevron-circle-right text-3xl ml-2 relative -bottom-5 -right-1/4 lg:hidden" onClick={handleNext} />
         <i className="fa-solid fa-circle-info" />
@@ -72,7 +73,7 @@ const isMobile = window.matchMedia("only screen and (max-width: 1024px)").matche
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth="2"
+                strokeWidth="1.5"
                 d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
               />
             </svg>
@@ -91,7 +92,7 @@ const isMobile = window.matchMedia("only screen and (max-width: 1024px)").matche
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth="2"
+                strokeWidth="1.5"
                 d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
@@ -99,7 +100,7 @@ const isMobile = window.matchMedia("only screen and (max-width: 1024px)").matche
         </Link>
         <span className={"lg:hidden absolute top-24 right-3 sm:right-4"}>
           <svg xmlns="http://www.w3.org/2000/svg" className="text-balance-sshades-desat cursor-pointer w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={!mobileFullyVisible ? "M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" : "M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"} />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d={!mobileFullyVisible ? "M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" : "M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"} />
           </svg>
         </span>
       </div>
