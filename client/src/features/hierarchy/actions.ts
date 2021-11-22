8fce62b409aba09f462988f7bce1e9c1f43c764f
Nick Stebb <n.stebbings@gmail.com>
2021-11-22 18:10:29 +1300

    Dispatch node fetchREST after hierarchy load

--- client/src/features/hierarchy/actions.ts
+++ client/src/features/hierarchy/actions.ts
@@ -1,16 +1,24 @@
 import { clientRoutes } from "services/restApis";
 import { createCrudActionCreators } from "app/store_utils";
-
+import { fetchNodesREST } from "features/node/actions";
 const FETCH_HABIT_TREE = "fetch_habit_tree";
 
 export const actionStrings = [FETCH_HABIT_TREE];
 
 let clientRouteDict = {
-  show_all: ({ domainId, dateId }) =>
+  show_all: ({ domainId, dateId }: any): Promise<any> =>
     clientRoutes(
       `/habit_trees?domain_id=${domainId}&date_id=${dateId}`
     ).show_all(),
 };
+
+const fetchRoute = clientRouteDict.show_all.bind({});
+
+clientRouteDict.show_all = async ({ domainId, dateId }: any, thunkAPI: any) =>
+  fetchRoute({ domainId, dateId }).then((response: any) => {
+    thunkAPI.dispatch(fetchNodesREST()); // Populate Nodes
+    return thunkAPI.fulfillWithValue(response);
+  });
 
 const thunkCallBacks = Object.values(clientRouteDict);
 
