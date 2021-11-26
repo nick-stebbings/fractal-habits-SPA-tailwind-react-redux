9f072b2f116355bc822f8fba48da9357e52998a9
Nick Stebb <n.stebbings@gmail.com>
2021-11-27 12:14:00 +1300

    Created pedac for node accumulating algo

--- client/src/features/habitDate/reducer.ts
+++ client/src/features/habitDate/reducer.ts
@@ -17,6 +17,23 @@ export const initialState: Dictionary<HabitDate[]> = {
   },
 };
 
+// P:
+//     When a habit has been completed in the past as an atom, and it gets consequently subdivided, it's recorded state will be completed.
+
+//     However, there is an upwards cascading logic for completing habits, such that for a habit to be completed at a high level it must have all ancestor habits also completed.
+
+// Devise a consistent way of maintaining the 'previous completion state' of a previous atomic habit.
+
+// RULES:
+
+// If a habit was historically completed as an atom, that day's tree should commemorate that. (it should be green)
+
+// If a habit is green on the tree it should be green in the calendar widget and the same with other colours.
+
+// If a completed habit has new child nodes added, it should become 'semi completed'. This means it can only achieve full completion once all ancestor nodes are complete.
+
+// If a habit didn't exist on a day, it should not be rendered
+
 export const habitDateSlice = createSlice({
   name: "habitDate",
   initialState,
