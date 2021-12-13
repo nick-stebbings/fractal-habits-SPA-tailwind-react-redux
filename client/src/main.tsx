bc6828e9bd40262b61d3b1f3fdaa8a9c2d3235ff
Nick Stebb <nickolarse@riseup.net>
2021-12-13 17:55:52 +1300

    Same

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -34,6 +34,7 @@ import { Routes } from "./routes/Routes";
 
 // Import our CSS
 import "./styles/bundled-styles.css"; 
+import "./styles/bundled-styles.css"; 
 // import "./assets/styles/app-base.pcss";
 // import "./assets/styles/app-utils.pcss";
 // import "./assets/styles/app-components.pcss";
