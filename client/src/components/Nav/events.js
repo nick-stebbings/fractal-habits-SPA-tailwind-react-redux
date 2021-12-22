6328c2628d85511599c3e7e7dcc742f2021b9ca2
Nick Stebb <nickolarse@riseup.net>
2021-12-22 15:29:27 +1300

    Tweak swipe conditons

--- client/src/components/Nav/events.js
+++ client/src/components/Nav/events.js
@@ -1,16 +1,18 @@
 import Hammer from "hammerjs";
 import { isTouchDevice } from "app/helpers";
 
+export const Swipe = new Hammer.Swipe();
+
 const addSwipeGestures = function () {
-  const swipeBase = document.querySelector("#vis");
+  const swipeBase = document.querySelector("#app");
   const manager = new Hammer.Manager(swipeBase);
-  const Swipe = new Hammer.Swipe();
   const nextDate = document.querySelector(".fa-chevron-circle-right");
   const prevDate = document.querySelector(".fa-chevron-circle-left");
 
   manager.add(Swipe);
   manager.on("swipe", (e) => {
     if (Math.abs(e.deltaX) > 500) {
+      e.srcEvent.preventDefault();
       e.deltaX > 0 ? prevDate.click() : nextDate.click();
     }
   });
