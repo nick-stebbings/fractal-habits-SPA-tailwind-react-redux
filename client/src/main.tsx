9eb10356ea70be83dffdeeb250403b1232b2779e
Nick Stebb <nickolarse@riseup.net>
2022-01-05 12:52:53 +1300

    Remove debug code from main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,33 +1,3 @@
-window._p = function customLog(message, objs, color='black') {
-     switch (color) {
-         case 'success':  
-              color = 'Green'
-              break
-         case '!':     
-                 color = 'pink'  
-              break;
-         case 'info':     
-                 color = 'Yellow'  
-              break;
-         case 'error':   
-              color = 'Red'   
-              break;
-         case 'warning':  
-              color = 'Orange' 
-              break;
-         default: 
-              color = color
-     }
-     if (!import.meta.env.PROD) {
-     
-          console.log(`%c${message}`, `color:${color}`)
-          typeof objs == 'object' && console.table(objs);
-     }
-}
-import { debounce } from "app/helpers";
-window.FlashMessage.warning = debounce(window.FlashMessage.warning.bind(window), 700)
-
-
 import React from "react";
 import { render } from "react-dom";
 import { store } from "./app/store";
@@ -38,7 +8,7 @@ import { Routes } from "./routes/Routes";
 import "./styles/bundle.css";
 
 // Vendor CSS
-import "../.vscode/styles/vendor/flashJS/import";
+import "./styles/vendor/flashJS/import";
 
 // Vendor JS
 import "./assets/scripts/vendor/flash.min.js";
